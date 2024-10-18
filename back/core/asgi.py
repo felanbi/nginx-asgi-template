eae891abdc377dadcf84257dd5b2deeb80769081
Fayssal <elanbifayssal@gmail.com>
2024-10-19 01:29:13 +0200

    change api docs url

--- back/core/asgi.py
+++ back/core/asgi.py
@@ -11,7 +11,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.conf import settings
 
-app = FastAPI()
+app = FastAPI(
+    docs_url='/api/docs'
+)
 
 register_tortoise(
     app,
