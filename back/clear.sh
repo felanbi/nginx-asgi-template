1241be0a35490658d41ad4c465f4ee61197a7231
Fayssal <elanbifayssal@gmail.com>
2024-10-26 18:33:10 +0200

    Add dev mode for backend

--- back/clear.sh
+++
@@ -1,2 +0,0 @@
-find **/migrations -type f ! -name __init__.py | xargs rm -f
-rm -f db.sqlite3
--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 manage.py makemigrations && python3 manage.py migrate 
 python3 manage.py collectstatic -l --noinput
-exec "$@"
+exec uvicorn core.asgi:app --host 0.0.0.0
